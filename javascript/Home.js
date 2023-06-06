98424ada19eadc338b88974fb1a25f9cf6ca479d
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2023-06-06 21:39:56 +0300

    My commit message

--- javascript/Home.js
+++ javascript/Home.js
@@ -2,4 +2,4 @@ let apiQuestApi = new TempApi.QuestApi();import TempApi from '../src/index';let 
       function () { $("#datepicker").datepicker({format: 'dd-mm-yyyy'}); }
     );document.getElementById('iyrq2').onclick = (event) => {
     event.preventDefault();
-    quest['paperD'] = document.querySelector("[annotationname = 'paperD']").value;quest['lcodetools'] = document.querySelector("[annotationname = 'lcodetools']").value;apiQuestApi.createquest( quest, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); }});};window.onload = () => {};
+    quest['paperD'] = document.querySelector("[annotationname = 'paperD']").value;quest['lcodetools'] = document.querySelector("[annotationname = 'lcodetools']").value;apiQuestApi.createquest( quest, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); {   location.href= '/viewresults' ;}}});};window.onload = () => {};
